c411873922e084d2757eee9b3ba12e14d893ca23
rstepien <67024204+rstepien@users.noreply.github.com>
2024-02-09 15:46:35 +0100

    passing mvn variables

--- src/main/java/pl/aqaclickuprs/properties/ClickupProperties.java
+++ src/main/java/pl/aqaclickuprs/properties/ClickupProperties.java
@@ -2,17 +2,27 @@ package pl.aqaclickuprs.properties;
 
 import java.util.ResourceBundle;
 
+import static java.lang.System.getProperty;
+
 public class ClickupProperties {
 
     private static final String TOKEN = "clickup.token";
     private static final String TEAM_ID = "clickup.team.id";
 
     public static String getToken() {
-        return getProperty(TOKEN);
+        if (getProperty(TOKEN).isEmpty() || getProperty(TOKEN).startsWith("your")) {
+            return System.getProperty("TOKEN");
+        } else {
+            return getProperty(TOKEN);
+        }
     }
 
     public static String getTeamId() {
-        return getProperty(TEAM_ID);
+        if (getProperty(TEAM_ID).isEmpty() || getProperty(TEAM_ID).startsWith("your")) {
+            return System.getProperty("TEAM_ID");
+        } else {
+            return getProperty(TEAM_ID);
+        }
     }
 
     private static String getProperty(String key) {
